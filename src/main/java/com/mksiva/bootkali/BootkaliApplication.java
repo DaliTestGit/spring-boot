2bce12a16f1010fceb3b6ff4bc408b5c43be4e8e
Siva Kalidasan <pkt.siva@gmail.com>
2017-09-03 19:15:12 +0530

    updated pom.xml

--- src/main/java/com/mksiva/bootkali/BootkaliApplication.java
+++ src/main/java/com/mksiva/bootkali/BootkaliApplication.java
@@ -1,36 +1,27 @@
 package com.mksiva.bootkali;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class BootkaliApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(applicationClass, args);
-	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
+    public static void main(String[] args) {
+        SpringApplication.run(BootkaliApplication.class, args);
     }
- 
-    private static Class<BootkaliApplication> applicationClass = BootkaliApplication.class;
-    
-    
-	@RestController
-	class HelloController {
-	 
-	    @RequestMapping("/hello/{name}")
-	    String hello(@PathVariable String name) {
-	         
-	             return "Hi "+name+", Welcome!";
-	              
-	    }
-	 }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BootkaliApplication.class);
+    }
 }
+
