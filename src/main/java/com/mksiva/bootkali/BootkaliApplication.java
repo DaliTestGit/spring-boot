be9217a2a693b91a93e85cc2730d61c46c00dbe0
Siva Kalidasan <pkt.siva@gmail.com>
2017-09-03 20:12:05 +0530

    added index.html & pom.xml update

--- src/main/java/com/mksiva/bootkali/BootkaliApplication.java
+++ src/main/java/com/mksiva/bootkali/BootkaliApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,13 +22,6 @@ public class BootkaliApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BootkaliApplication.class);
-    }
-    
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = 
-                      new TomcatEmbeddedServletContainerFactory();
-        return factory;
-     }
+    }   
 }
 
